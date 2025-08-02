c1c7b1d0cade9dd54cd529215a21f2e9a5853b35
meet borad <22bmiit030@gmail.com>
2025-08-02 20:56:47 +0530

    fix: added redirects file to fix Vercel 404

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/_redirects", // path to your redirects file
+          dest: ".",                // puts it in dist/
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
