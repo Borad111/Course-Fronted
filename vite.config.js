db5f8ffed3a4269aace8e0b3868e3f592551deca
meet borad <22bmiit030@gmail.com>
2025-08-02 21:14:33 +0530

    Delete vit plugin

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,9 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects", // path to your redirects file
-          dest: ".",                // puts it in dist/
-        },
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
